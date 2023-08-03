4b01e2046a3dcd40d1fd5181813cb31d444bb7e2
Mert Çetin <mrtctn506@gmail.com>
2023-08-03 15:20:55 +0300

    app and noteForm finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import NoteForm from './components/NoteForm'
 import NoteList from './components/NoteList'
+import { useState } from 'react'
 
 function App() {
 
+  const [notes, setNotes] = useState([]);
+
+  const onAdd = (note) => {
+    setNotes([...notes, note])
+  }
 
   return (
     <div>
-      <NoteForm />
+      <h1>Not Defteri Uygulaması</h1>
+      <NoteForm onSubmit={onAdd} />
       <NoteList />
     </div>
   )
--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 
-function NoteForm() {
+function NoteForm({ onSubmit }) {
+
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (title !== '' && content !== '') {
+            onSubmit({ title, content })
+            setTitle('');
+            setContent('');
+        }
+    };
+
     return (
-        <div>NoteForm</div>
+        <form onSubmit={handleSubmit}>
+            <input type='text' value={title} placeholder='Title' onChange={(e) => e.target.value} />
+            <textarea placeholder='Write Here...' value={content} onChange={(e) => e.target.value} />
+            <button type='submit'>Add</button>
+        </form>
     )
 }
 
