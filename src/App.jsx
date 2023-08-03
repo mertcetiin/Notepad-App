ff2cb0821c4483d54de4671be42d93b015144cd3
Mert Çetin <mrtctn506@gmail.com>
2023-08-03 14:34:30 +0300

    vite and components files loaded

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NoteForm from './components/NoteForm'
+import NoteList from './components/NoteList'
+
+function App() {
+
+
+  return (
+    <div>
+      <NoteForm />
+      <NoteList />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NoteForm() {
+    return (
+        <div>NoteForm</div>
+    )
+}
+
+export default NoteForm;
